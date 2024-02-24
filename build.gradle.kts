31f7e5f705a8de8a41fa0404b4747a6ebfb4d63a
ThinaticSystem <git@thinaticsystem.com>
2024-02-24 14:31:06 +0900

    env dep jackson (Kotlin)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     val awsLambdaJavaCoreVersion: String by project
     implementation("com.amazonaws:aws-lambda-java-core:$awsLambdaJavaCoreVersion")
+    val jacksonModuleKotlinVersion: String by project
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonModuleKotlinVersion")
 }
 
 tasks.test {
