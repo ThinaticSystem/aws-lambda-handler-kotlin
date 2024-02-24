1db7c3de5ba8fc9fd1a44e303520d559cd8904d7
ThinaticSystem <git@thinaticsystem.com>
2024-02-24 14:33:01 +0900

    feat RequestKotlinHandler

---
+++ src/main/kotlin/RequestKotlinHandler.kt
@@ -0,0 +1,33 @@
+package com.thinaticsystem
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.jsonMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
+import java.io.InputStream
+import java.io.OutputStream
+
+abstract class RequestKotlinHandler<I, O>(
+    private val inputClazz: Class<I>,
+    private val outputClazz: Class<O>,
+    private val jsonMapper: JsonMapper = defaultJsonMapper,
+) : RequestStreamHandler {
+    companion object {
+        val defaultJsonMapper = jsonMapper {
+            addModule(kotlinModule())
+        }
+    }
+
+    abstract fun handleRequest(input: I): O
+
+    override fun handleRequest(
+        inputStream: InputStream,
+        outputStream: OutputStream,
+        context: Context,
+    ) {
+        val input = this.jsonMapper.readValue(inputStream, this.inputClazz)
+        val output = this.handleRequest(input)
+        this.jsonMapper.writeValue(outputStream, output)
+    }
+}
