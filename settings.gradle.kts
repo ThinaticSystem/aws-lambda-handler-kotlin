4795eacd03fc814e8076dc5861a3e5af2bd63a27
ThinaticSystem <git@thinaticsystem.com>
2024-02-24 13:52:53 +0900

    rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,4 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "aws-lambda-handler-kotlin"
+rootProject.name = "template-aws-lambda-handler-kotlin"
